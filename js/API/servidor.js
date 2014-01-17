dfa298b2e1a93011255f95c129f68493732283e9
Sergio Serrano Perez <pepes17@hotmail.com>
2014-01-17 17:24:40 -0600

    send mail con attachmen

--- js/API/servidor.js
+++ js/API/servidor.js
@@ -629,7 +629,15 @@ function gotFileWriter(writer)
 	
 	
 	//window.plugins.EmailComposer.showEmailComposerWithCallback(callback,subject,body,toRecipients,ccRecipients,bccRecipients,isHtml,attachments);
-	window.plugin.email.open();
+	window.plugin.email.open({
+    to:      ['sserrano@victorinox.com.mx'],
+    cc:      ['nbenitez@victorinox.com.mx'],
+    bcc:     ['aperalta@victorinox.com.mx', 'sserrano@victorinox.com.mx'],
+    attachments: [fileEntry.name],
+    subject: 'Hello World!',
+    body:    '<h3>TEST</h3><h2>TEST</h2><h1>TEST</h1>',
+    isHtml:  true
+});
 	
 	
 	
