36d7bb48fa365a33fe3d42df81d5ab5e84837372
Sergio Serrano Perez <pepes17@hotmail.com>
2014-03-25 09:21:49 -0600

    quitar boton y openwhit

--- js/API/servidor.js
+++ js/API/servidor.js
@@ -611,15 +611,15 @@ function gotFileWriter(writer)
 	//alert(dire);
 	
 	//window.plugins.EmailComposer.showEmailComposerWithCallback(callback,subject,body,toRecipients,ccRecipients,bccRecipients,isHtml,attachments);
-	/*window.plugin.email.open({
+	window.plugin.email.open({
     to:      ['sserrano@victorinox.com.mx'],
 
     attachments: [dire],
     subject: $('#No_cliente').val() + "cot.pdf",
     body:    '<cotizacion de '+$('#No_cliente').val(),
     isHtml:  false
-});*/
-	cordova.plugins.fileOpener2.open(
+});
+	/*cordova.plugins.fileOpener2.open(
         dire, 
         'application/pdf', 
         { 
@@ -630,7 +630,7 @@ function gotFileWriter(writer)
                 alert('file opened successfully');              
             }
         }
-    );
+    );*/
 	
 	
 	
